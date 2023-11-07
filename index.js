9947da746ab40eb448ca04538ee951c3b251e3c9
Souradeep <souradeepghosh00@gmail.com>
2023-11-08 03:03:21 +0530

    added multiuser functionality

--- index.js
+++ index.js
@@ -40,9 +40,26 @@ let total = 0;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let currentUserId = 1;
+
+let users = [];
+
+function getCurrentUser() {
+  let cur_user = users.find((user) => {
+    return user.id == currentUserId;
+  });
+  return cur_user;
+}
+
 app.get("/",async (req, res) => {
-  const result = await db.query("SELECT country_code FROM visited_countries");
-  console.log(result.rows);
+  visited_countries = [];
+  let data = await db.query("SELECT * FROM users");
+  users = data.rows;
+  console.log(users);
+  let cur_user = getCurrentUser();
+  console.log(cur_user);
+  const result = await db.query("SELECT country_code FROM visited_countries WHERE user_id = $1",[currentUserId]);
+  //console.log(result.rows);
   console.log(result.rowCount);
   total = result.rowCount;
   result.rows.forEach((record) => {
@@ -51,8 +68,33 @@ app.get("/",async (req, res) => {
   console.log(visited_countries);
   res.render("index.ejs", { 
     total : total, 
-    countries : visited_countries
+    countries : visited_countries,
+    users: users,
+    color: cur_user.color,
   });
+});
+
+app.post("/user",(req,res) => {
+  if(req.body.add==='new') {
+    res.render("new.ejs");
+  } else {
+    currentUserId = req.body.user;
+    // console.log("hello",typeof(req.body.user));
+    res.redirect("/");
+  }
+});
+
+app.post("/new",async(req,res) => {
+  let name = req.body.name;
+  let color = req.body.color;
+  try {
+    let new_id = await db.query("INSERT INTO users (name, color) VALUES ($1,$2) RETURNING id",[name,color]);
+    console.log(new_id.rows);
+    currentUserId = new_id.rows[0].id;
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/add",async(req,res) => {
@@ -65,7 +107,7 @@ app.post("/add",async(req,res) => {
     //res.redirect("/");
     if(code.rows.length !== 0) {
       try {
-        await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[code.rows[0].country_code]);
+        await db.query("INSERT INTO visited_countries (country_code, user_id) VALUES ($1,$2)",[code.rows[0].country_code,currentUserId]);
         res.redirect("/");
       } catch(err) {
         console.log(err);
